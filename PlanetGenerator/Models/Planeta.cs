07dcab7f6487a30ef5f25ed5a895d721d2f13513
czM1K3 <misamadera1@gmail.com>
2020-10-03 11:13:50 +0200

    Added some Linq methods

--- PlanetGenerator/Models/Planeta.cs
+++ PlanetGenerator/Models/Planeta.cs
@@ -56,6 +56,13 @@ namespace PlanetGenerator.Models
             Z = rnd.Next();
         }
 
+        public Souradnice(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
         public int X { get; private set; }
         public int Y { get; private set; }
         public int Z { get; private set; }
--- PlanetGenerator/Program.cs
+++ PlanetGenerator/Program.cs
@@ -12,8 +12,15 @@ namespace PlanetGenerator
         static void Main(string[] args)
         {
             var sequenceRaw = GenerujPlanety();
-            var sequence10First = sequenceRaw.Take(20);
-            foreach (var item in sequence10First)
+
+            var sequence10PlanetSZivotem = sequenceRaw.ExistujeZivot(10);
+            var sequenceBlizkoZeme = sequenceRaw.BlizkoZeme(10000,10);
+            var sequenceVyhledatString = sequenceRaw.VyhledatString("ABC",1);
+            var sequenceVyhledatSouradnice = sequenceRaw.VyhledatSouradnice(new Souradnice(123465789, 123465789,123465789),1000000);
+            var sequenceUID = sequenceRaw.VyhledatUID(50);
+            
+
+            foreach (var item in sequenceUID)
             {
                 item.Vypis();
             }
@@ -33,9 +40,41 @@ namespace PlanetGenerator
 
     static class MyExtensionUtils
     {
-        public static IEnumerable<Planeta> ExistujeZivot(this IEnumerable<Planeta> seq)
+        public static IEnumerable<Planeta> ExistujeZivot(this IEnumerable<Planeta> seq, int count)
         {
-            return seq.Where(e => e.ExistujeZivot);
+            return seq.Where(e => e.ExistujeZivot).Take(count);
+        }
+
+        public static IEnumerable<Planeta> BlizkoZeme(this IEnumerable<Planeta> seq, int vzdalenost, int count)
+        {
+            return seq.Where(e => e.VzdalenostOdZeme <= vzdalenost).Take(count);
+        }
+
+        public static IEnumerable<Planeta> VyhledatString(this IEnumerable<Planeta> seq, string input, int count)
+        {
+            return seq.Where(e => e.Nazev.Split('-')[0] == input ).Take(count);
+        }
+
+        public static IEnumerable<Planeta> VyhledatInt(this IEnumerable<Planeta> seq, int input, int count)
+        {
+            return seq.Where(e => Convert.ToInt32(e.Nazev.Split('-')[1]) == input).Take(count);
+        }
+
+        public static IEnumerable<Planeta> VyhledatSouradnice(this IEnumerable<Planeta> seq, Souradnice cordinates, int tolerance)
+        {
+            return seq.Where(e =>
+            e.Pozice.X > cordinates.X - tolerance &&
+            e.Pozice.X < cordinates.X + tolerance &&
+            e.Pozice.Y > cordinates.Y - tolerance &&
+            e.Pozice.Y < cordinates.Y + tolerance &&
+            e.Pozice.Z > cordinates.Z - tolerance &&
+            e.Pozice.Z < cordinates.Z + tolerance
+            ).Take(1);
+        }
+
+        public static IEnumerable<Planeta> VyhledatUID(this IEnumerable<Planeta> seq, int id)
+        {
+            return seq.Where(e =>e.UID == id).Take(1);
         }
     }
 }
